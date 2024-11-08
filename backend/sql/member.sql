f4a99f4bc876e02d8222514463504c38ac24251b
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-08 23:00:03 +0900

    fix: Fix SQL
    Fix condition of columns

--- backend/sql/member.sql
+++ backend/sql/member.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS `members`
     `id`          BIGINT PRIMARY KEY,
     `nickname`    VARCHAR(100) NOT NULL,
     `email`       VARCHAR(255) NOT NULL,
-    `exp`         INT          NOT NULL DEFAULT 0,
+    `exp`         INT      DEFAULT 0,
     `uuid`        VARCHAR(255) NOT NULL,
     `profile_url` VARCHAR(1000),
     `tier`        VARCHAR(50)  NOT NULL,
-    `create_at`   DATETIME     NOT NULL DEFAULT now(),
-    `delete_at`   DATETIME     NULL     DEFAULT NULL
+    `create_at`   DATETIME DEFAULT now(),
+    `delete_at`   DATETIME DEFAULT NULL
 )
     ENGINE = InnoDB;

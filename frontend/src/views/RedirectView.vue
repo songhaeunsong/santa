de0bd445c89898b8cd89b424719dd66501f4b613
songhaeunsong <gkdms3469@naver.com>
2024-11-09 20:09:59 +0900

    feat: 로그인 - 리다이렉트 페이지 구현

---
+++ frontend/src/views/RedirectView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { usePostCodeApi } from '../api/authentication';
+
+const route = useRoute();
+const code = ref<string>(route.query.code as string);
+
+const postCode = usePostCodeApi();
+
+onMounted(() => {
+  if (code.value) {
+    postCode(code.value);
+  }
+});
+</script>
+
+<template>
+  <div class="text-xl text-deepGreen">loading...</div>
+</template>

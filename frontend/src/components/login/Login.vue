b9fb4018421cce774c994b24f2cdccdf12720f9b
songhaeunsong <gkdms3469@naver.com>
2024-11-09 20:08:59 +0900

    feat: 로그인 페이지 구현

---
+++ frontend/src/components/login/Login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const kakaoUrl = `${import.meta.env.VITE_OAUTH_KAKAO_URL}?response_type=code&client_id=${import.meta.env.VITE_OAUTH_KAKAO_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_OAUTH_KAKAO_REDIRECT_URI}`;
+</script>
+
+<template>
+  <div class="w-[350px] flex flex-col justify-between h-[35%] gap-20">
+    <div class="flex flex-col items-center logoContainer jusify-center">
+      <img
+        class="w-[160px]"
+        src="/santaLogo.png"
+        alt="santa" />
+      <span class="font-bold text-[50px] tracking-wide text-deepGreen"
+        >SANTA</span
+      >
+    </div>
+    <div>
+      <a :href="kakaoUrl"
+        ><img
+          src="/kakao_login_large_wide.png"
+          alt="kakao-login"
+      /></a>
+    </div>
+  </div>
+</template>

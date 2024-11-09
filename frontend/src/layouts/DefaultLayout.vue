27862d164d6275bf05be54db981fda77f650a626
songhaeunsong <gkdms3469@naver.com>
2024-11-09 20:04:43 +0900

    feat: 로그인 라우터 설정

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CNNavigationMenu from '../components/ui/cnComponents/CNNavigationMenu.vue';
+</script>
 
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="flex flex-col min-h-screen">
+    <div
+      class="h-[60px] bg-santaIvory flex justify-between p-8 items-center text-deepGreen sticky top-0 z-10 shadow">
+      <router-link to="/">
+        <span class="text-2xl font-bold">SANTA</span>
+      </router-link>
+      <div class="flex items-center gap-10">
+        <CNNavigationMenu />
+        <router-link to="/login">로그인</router-link>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center justify-center flex-1 w-full overflow-auto bg-santaWhite">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-import HomeView from '../components/HomeView.vue';
-import HikeView from '../components/HikeView.vue';
-import RankView from '../components/RankView.vue';
 import DefaultLayout from '../layouts/DefaultLayout.vue';
+import HomeView from '../views/HomeView.vue';
+import HikeView from '../views/HikeView.vue';
+import RankView from '../views/RankView.vue';
+import LoginView from '../views/LoginView.vue';
+import RedirectView from '../views/RedirectView.vue';
+import SignupInitView from '../views/SignupInitView.vue';
+import SimpleLayout from '../layouts/SimpleLayout.vue';
 
 const routes: readonly RouteRecordRaw[] = [
   {
@@ -20,6 +24,21 @@ const routes: readonly RouteRecordRaw[] = [
     path: '/rank',
     component: DefaultLayout,
     children: [{ path: '', component: RankView }]
+  },
+  {
+    path: '/login',
+    component: DefaultLayout,
+    children: [{ path: '', component: LoginView }]
+  },
+  {
+    path: '/login/init',
+    component: SimpleLayout,
+    children: [{ path: '', component: SignupInitView }]
+  },
+  {
+    path: '/oauth/redirect',
+    component: DefaultLayout,
+    children: [{ path: '', component: RedirectView }]
   }
 ];
 

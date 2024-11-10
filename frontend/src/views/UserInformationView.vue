0ac42115eed804b0db2e4096163e379e459f3859
songhaeunsong <gkdms3469@naver.com>
2024-11-10 17:43:36 +0900

    feat: 마이페이지 컴포넌트 연결

--- frontend/src/views/UserInformationView.vue
+++ frontend/src/views/UserInformationView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserInformation from '../components/user/UserInformation.vue';
+</script>
 
 <template>
-  <div>userInformation</div>
+  <UserInformation />
 </template>

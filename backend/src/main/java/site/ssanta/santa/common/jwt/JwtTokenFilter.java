1b83bc4a725b97dcbcb1327cfd32f60aa5f29d1a
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-10 21:53:17 +0900

    fix: Fix cors error
    Fix cors error passing preflight

--- backend/src/main/java/site/ssanta/santa/common/jwt/JwtTokenFilter.java
+++ backend/src/main/java/site/ssanta/santa/common/jwt/JwtTokenFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -45,6 +46,12 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        if (HttpMethod.OPTIONS.name().equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         try {
             String token;
             Cookie[] cookies = request.getCookies();

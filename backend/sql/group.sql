6d8a00e824252041f507a908a5890e91e91138ac
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-13 02:15:21 +0900

    feat: Add attribute
    Add attribute count of member to use in page

--- backend/sql/group.sql
+++ backend/sql/group.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS `groups`
 (
-    `id`          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `name`        VARCHAR(30)  NOT NULL,
-    `exp`         BIGINT   DEFAULT 0,
-    `admin_id`    BIGINT,
-    `description` VARCHAR(100),
-    `create_at`   DATETIME DEFAULT now(),
-    `delete_at`   DATETIME DEFAULT NULL,
-    FOREIGN KEY(admin_id) REFERENCES members(id)
-    ON DELETE CASCADE
+    `id`               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name`             VARCHAR(30) NOT NULL,
+    `exp`              BIGINT   DEFAULT 0,
+    `admin_id`         BIGINT,
+    `description`      VARCHAR(100),
+    `count_of_members` BIGINT   DEFAULT 1,
+    `create_at`        DATETIME DEFAULT now(),
+    `delete_at`        DATETIME DEFAULT NULL,
+    FOREIGN KEY (admin_id) REFERENCES members (id)
+        ON DELETE CASCADE
 )
     ENGINE = InnoDB;

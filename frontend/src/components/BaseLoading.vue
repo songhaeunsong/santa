c3ddab92a1c8bfd103a6907c2eaa3b20e80c7d04
songhaeunsong <gkdms3469@naver.com>
2024-11-14 11:24:03 +0900

    feat: 스피너 기능

---
+++ frontend/src/components/BaseLoading.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { VueSpinnerBox } from 'vue3-spinners';
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-[90vh]">
+    <VueSpinnerBox
+      size="80"
+      color="#435E49" />
+  </div>
+</template>

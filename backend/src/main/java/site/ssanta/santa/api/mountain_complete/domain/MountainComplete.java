2f6be2d6279276321c2a9e050cdc481a7e937e20
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-17 03:23:18 +0900

    feat: Add entity
    Add mountain complete entity using later

---
+++ backend/src/main/java/site/ssanta/santa/api/mountain_complete/domain/MountainComplete.java
@@ -0,0 +1,22 @@
+package site.ssanta.santa.api.mountain_complete.domain;
+
+import jakarta.persistence.*;
+import site.ssanta.santa.api.member.domain.Member;
+import site.ssanta.santa.api.mountain.domain.Mountain;
+
+@Entity
+@Table(name = "mountain_completes")
+public class MountainComplete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mountain_id")
+    private Mountain mountain;
+}

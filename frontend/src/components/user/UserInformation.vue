9099aa082e8ae7ab365e41f5e5454984a07f06cf
songhaeunsong <gkdms3469@naver.com>
2024-11-17 00:15:39 +0900

    feat: 회원 정보 조회 기능 user-view  연결

--- frontend/src/components/user/UserInformation.vue
+++ frontend/src/components/user/UserInformation.vue
@@ -1,37 +1,33 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import CNProgress from '../ui/cnComponents/CNProgress.vue';
 import MountainTab from './userMountain/MountainTab.vue';
+import { useGetMemberImpormation } from '../../api/member/information';
 
-const user = ref({
-  exp: 30,
-  profileUrl: '/santaLogo.png',
-  tier: 'BRONZE',
-  nickname: '하은',
-  email: 'gkdms3469@gmail.com'
-});
+const { data: memberInformation, isError } = useGetMemberImpormation();
 </script>
 
 <template>
-  <div class="w-[100%] px-[30px] py-[50px] bg-santaGreen gap-5 flex">
-    <div class="w-[200px] h-[200px] bg-white rounded flex-shrink-0">
+  <div
+    v-if="!isError && memberInformation"
+    class="w-[100%] px-[200px] py-[100px] bg-santaBeige gap-8 flex">
+    <div class="w-[200px] h-[200px] bg-white rounded-[50%] flex-shrink-0">
       <img
-        :src="user.profileUrl"
+        :src="memberInformation.profileUrl"
         alt="user-profile"
-        class="object-cover w-full h-full rounded" />
+        class="object-cover w-full h-full rounded-[50%]" />
     </div>
     <div
-      class="flex flex-col justify-between gap-6 infoDiv text-santaWhite w-[100%]">
+      class="flex flex-col justify-between gap-6 infoDiv text-santaGreen w-[100%] py-8">
       <div>
-        <p class="text-2xl font-semibold">{{ user.nickname }}</p>
-        <p class="py-1 font-normal">{{ user.email }}</p>
+        <p class="text-3xl font-bold">{{ memberInformation.nickname }}</p>
+        <p class="py-1 font-normal">{{ memberInformation.email }}</p>
       </div>
       <div class="flex items-center gap-2 w-[100%]">
         <p
-          class="px-2 py-1 mr-3 text-xs font-medium bg-santaWhite text-deepGreen rounded-xl">
-          {{ user.tier }}
+          class="px-2 py-1 mr-3 text-xs font-medium bg-santaWhite text-santaGreen rounded-xl">
+          {{ memberInformation.tier }}
         </p>
-        <CNProgress :progress-value="user.exp" />
+        <CNProgress :progress-value="memberInformation.exp" />
       </div>
     </div>
   </div>

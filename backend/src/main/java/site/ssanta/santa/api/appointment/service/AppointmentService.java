f80e0b5ef5fc2c49414654f86d89d76ba5a5ee31
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-17 03:21:56 +0900

    fix: Fix error
    Fix error issue about using Calendar instance default object's time is 9am

--- backend/src/main/java/site/ssanta/santa/api/appointment/service/AppointmentService.java
+++ backend/src/main/java/site/ssanta/santa/api/appointment/service/AppointmentService.java
@@ -1,9 +1,11 @@
 package site.ssanta.santa.api.appointment.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.ssanta.santa.api.appointment.domain.Appointment;
+import site.ssanta.santa.api.appointment.dto.AppointmentJoinRequestDto;
 import site.ssanta.santa.api.appointment.dto.MakeAppointmentRequestDto;
 import site.ssanta.santa.api.appointment.repository.AppointmentRepository;
 import site.ssanta.santa.api.mountain.domain.Mountain;
@@ -12,6 +14,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AppointmentService {
@@ -35,11 +38,28 @@ public class AppointmentService {
         calendar.setTime(date);
 
         calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
         Date start = calendar.getTime();
 
         calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
         Date end = calendar.getTime();
 
+        log.debug("start: {}, end: {}", start, end);
         return appointmentRepository.findByGroupIdAndMeetAtBetweenWithParticipants(groupId, start, end);
+    }
+
+    @Transactional
+    public Appointment join(AppointmentJoinRequestDto dto) {
+        Appointment appointment = appointmentRepository.findById(dto.getAppointmentId())
+                .orElseThrow();
+        appointment.join();
+        return appointment;
     }
 }

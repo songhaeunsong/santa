cc26e74c51701d2e9eaf37763fbc2f67e456e145
songhaeunsong <gkdms3469@naver.com>
2024-11-19 14:59:32 +0900

    feat: 사용자의 관심 산 정보 조회 기능

--- frontend/src/api/member/information.ts
+++ frontend/src/api/member/information.ts
@@ -2,14 +2,23 @@ import { useQuery } from '@tanstack/vue-query';
 import apiClient from '../apiClient';
 import { Member } from '../../types/types';
 
-const getMemberInformation = async (): Promise<Member> => {
-  return (await apiClient.get<Member>('member/mypage')).data;
-};
+const getMemberInformation = () =>
+  apiClient.get<Member>('member/mypage').then(res => res.data);
 
 export const useGetMemberImpormation = () => {
-  return useQuery<Member, Error>({
+  return useQuery({
     queryKey: ['memberInformation'],
     queryFn: () => getMemberInformation(),
     staleTime: 1000 * 60 * 10
   });
 };
+
+const getMemberMountainLike = () =>
+  apiClient.get('member/like').then(res => res.data);
+
+export const useGetMemberMountainLike = () =>
+  useQuery({
+    queryKey: ['mountainsLiked'],
+    queryFn: () => getMemberMountainLike(),
+    staleTime: 1000 * 60 * 1
+  });

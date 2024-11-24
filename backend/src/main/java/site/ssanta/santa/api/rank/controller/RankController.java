bff74c8315ef0453bdca780414bd8b6f9711184f
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-25 01:14:17 +0900

    feat: Add rank
    Add rank feature

--- backend/src/main/java/site/ssanta/santa/api/rank/controller/RankController.java
+++ backend/src/main/java/site/ssanta/santa/api/rank/controller/RankController.java
@@ -1,5 +1,9 @@
 package site.ssanta.santa.api.rank.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +15,8 @@ import site.ssanta.santa.api.group.dto.GroupVO;
 import site.ssanta.santa.api.group.service.GroupService;
 import site.ssanta.santa.api.member.dto.MemberInfoVO;
 import site.ssanta.santa.api.member.service.MemberService;
+import site.ssanta.santa.api.rank.dto.GroupRankResponseDto;
+import site.ssanta.santa.api.rank.dto.IndividualRankResponseDto;
 
 import java.util.List;
 
@@ -25,14 +31,22 @@ public class RankController {
     private final GroupService groupService;
 
     @GetMapping("/individual")
+    @Operation(summary = "개인 랭킹 조회", description = "개인 랭킹 조회, 티어 순, 경험치 순")
+    @ApiResponse(responseCode = "200", description = "개인 랭킹 조회 결과",
+    content = @Content(schema = @Schema(implementation = IndividualRankResponseDto.class)))
     public ResponseEntity<?> getIndividualRank() {
         List<MemberInfoVO> result = memberService.getMemberRank();
-        return ResponseEntity.ok().body(result);
+        IndividualRankResponseDto response = new IndividualRankResponseDto(result);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/group")
+    @Operation(summary = "그룹 랭킹 조회", description = "그룹 랭킹 조회, 경험치 순, 생성 순")
+    @ApiResponse(responseCode = "200", description = "그룹 랭킹 조회 결과",
+            content = @Content(schema = @Schema(implementation = GroupRankResponseDto.class)))
     public ResponseEntity<?> getGroupRank() {
         List<GroupVO> result = groupService.getGroupRank();
-        return ResponseEntity.ok().body(result);
+        GroupRankResponseDto response = new GroupRankResponseDto(result);
+        return ResponseEntity.ok().body(response);
     }
 }

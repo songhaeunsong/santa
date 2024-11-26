f2222cdc97d018a0cc2f3f5151f9376d9ca3a89e
songhaeunsong <gkdms3469@naver.com>
2024-11-26 09:59:43 +0900

    feat: mountain 상세 기능 및 완등한 산(get, post) 기능

--- frontend/src/api/mountain/mountain.ts
+++ frontend/src/api/mountain/mountain.ts
@@ -1,17 +1,21 @@
-import { useMutation } from '@tanstack/vue-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query';
 import apiClient from '../apiClient';
-import { Mountains } from '../../types/types';
+import { Complete, MountainDetail, Mountains } from '../../types/types';
 import { Province, provinceObj } from '../../data/CityData';
+import { usePostAppointment } from '../appointment/appointment';
 
-const getMountain = async (province: Province, city: string) =>
-  (
-    await apiClient.get<Mountains>('/mountain', {
+const getMountain = (province: Province, city: string) => {
+  const cityRequest = city === '전체' ? '' : city;
+
+  return apiClient
+    .get<Mountains>('/mountain', {
       params: {
         province: provinceObj[province],
-        city
+        city: cityRequest
       }
     })
-  ).data;
+    .then(res => res.data);
+};
 
 export const useGetMountain = () =>
   useMutation({
@@ -19,3 +23,57 @@ export const useGetMountain = () =>
       getMountain(province, city),
     onSuccess: () => {}
   });
+
+const getMountainDetail = (mountainCode: string) =>
+  apiClient
+    .get<MountainDetail>('/mountain/detail', {
+      params: {
+        mountainCode
+      }
+    })
+    .then(res => res.data);
+
+export const useGetMountainDetail = (mountainCode: string) =>
+  useQuery({
+    queryKey: ['mountainDetail'],
+    queryFn: () => getMountainDetail(mountainCode),
+    staleTime: 0,
+    gcTime: 0
+  });
+
+export interface GetMountainCompletedResponse {
+  completes: Complete[];
+}
+
+const getMountainCompleted = () =>
+  apiClient
+    .get<GetMountainCompletedResponse>('mountain/complete')
+    .then(res => res.data);
+
+export const useGetMountainCompleted = () =>
+  useQuery({
+    queryKey: ['mountainsCompleted'],
+    queryFn: () => getMountainCompleted(),
+    staleTime: 1000 * 60 * 10
+  });
+
+interface PostMountainCompletedResponse {
+  exp: number;
+}
+
+const postMountainCompleted = (mountainId: number) =>
+  apiClient
+    .post<PostMountainCompletedResponse>('mountain/complete', { mountainId })
+    .then(res => res.data);
+
+export const usePostMountainCompleted = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (mountainId: number) => postMountainCompleted(mountainId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['mountainsCompleted', 'mountainDetail']
+      });
+    }
+  });
+};

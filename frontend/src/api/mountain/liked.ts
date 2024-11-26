1f5a895bfae3c51f822fb87a3c65cdc131275099
songhaeunsong <gkdms3469@naver.com>
2024-11-26 09:58:57 +0900

    feat: 관심 기능

--- frontend/src/api/mountain/liked.ts
+++ frontend/src/api/mountain/liked.ts
@@ -1,11 +1,44 @@
-import { useMutation } from '@tanstack/vue-query';
+import { useMutation, useQueryClient } from '@tanstack/vue-query';
 import apiClient from '../apiClient';
 
 const postLiked = async (mountainId: number) =>
   (await apiClient.post('/mountain/like', { mountainId })).data;
 
 export const usePostLiked = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
-    mutationFn: (mountainId: number) => postLiked(mountainId)
+    mutationFn: (mountainId: number) => postLiked(mountainId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['mountainsLiked']
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['mountainDetail'],
+        refetchType: 'none'
+      });
+    }
+  });
+};
+
+const deleteLiked = async (mountainId: number) =>
+  await apiClient.delete('/mountain/like', { data: { mountainId } });
+
+export const useDeleteLiked = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (mountainId: number) => deleteLiked(mountainId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['mountainsLiked']
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: ['mountainDetail'],
+        refetchType: 'none'
+      });
+    }
   });
 };
